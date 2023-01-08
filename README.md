a7fe4c438c2c17cb2d2fc9f4745d305d678de09a
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2023-01-08 01:25:25 -0800

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# saturnbay_productions
-demo site
---
+++ app/head.js
@@ -0,0 +1,9 @@
+const Head = () => (
+  <>
+    <title>SaturnBay</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/SBLogoCirclePurple.png" />
+  </>
+);
+
+export default Head;
